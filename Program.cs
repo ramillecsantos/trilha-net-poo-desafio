23eebbcf5f9cff25ac3b6546aa8ce72a6b0e75f2
Ramille Cordeiro <cordeiro.ramillecs@gmail.com>
2024-01-07 19:47:09 -0300

    Desafio Celular POO concluído.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "988063205", modelo: "Nokia G11 Plus", imei: "12345", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "988063205", modelo: "Iphone 11 Pro", imei: "54321", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
